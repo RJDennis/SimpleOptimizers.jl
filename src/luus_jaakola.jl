7399efd88095a6333f966f4dca81cfdaec7bcae8
Richard Dennis <richard.dennis@glasgow.ac.uk>
2017-03-08 01:27:04 +0000

    Made Luus-Jaakola more robust

--- src/luus_jaakola.jl
+++ src/luus_jaakola.jl
@@ -1,33 +1,43 @@
-function luus_jaakola{T<:AbstractFloat,S<:Integer}(f::Function,x::Array{T,1},d::Array{T,1},range::Array{T,2},tol::T,maxiters::S)
+function luus_jaakola{T<:AbstractFloat,S<:Integer}(f::Function,x::Array{T,1},d::Array{T,1},seed::S,tol::T,maxiters::S)
+
+  # d is the radius of the initial search area about the initial x
+  # maxiters should be large, especially if is chosen to be too small
+
+  srand(seed)
 
   lambda = 0.95
+  d_store = d
+  x_old = copy(x)
 
   n = length(x)
 
-  len = Inf
-  iters   = 0
-  while true
+  len_outer = Inf
+  iters = 0
+  while len_outer > tol
+    len_inner = Inf
+    d = d_store
+    while len_inner > tol
 
-    for i = 1:n
-      d[i] = minimum([d[i],abs(x[i]-range[1,i]),abs(x[i]-range[2,i])])
-      d[i] = maximum([d[i],tol])
+      z = rand(n)
+      y = x - d + 2*z.*d
+
+      if f(y) < f(x)
+        len_inner = maximum(abs,[x-y; f(y)-f(x)])
+        x = y
+      else
+        d = lambda*d
+      end
+
+      iters += 1
+      if iters >= maxiters
+        break
+      end
+
     end
 
-    z = rand(n)
-    y = x - d + 2*z.*d
+    len_outer = maximum(abs,x-x_old)
+    x_old = copy(x)
 
-    if f(y) < f(x)
-      len = maximum(abs,[x-y; f(y)-f(x)])
-      x = y
-    else
-      d = lambda*d
-    end
-
-    if len < tol
-      break
-    end
-
-    iters += 1
     if iters >= maxiters
       break
     end
